ea05725237ffbab0a090800329ce464b0bd61f4e
Nat Young <nat.young7@gmail.com>
2015-10-28 16:01:04 +1100

    email forms

--- app/mailers/form_submission_mailer.rb
+++ app/mailers/form_submission_mailer.rb
@@ -1,2 +1,22 @@
 class FormSubmissionMailer < ApplicationMailer
+  def student_submission_notification(to_address, from_address)
+    mail( to: to_address,
+          from: from_address,
+          subject: 'New student form submission from careerseekers.org.au',
+          template_name: 'student_submission_notification' )
+  end
+
+  def professional_submission_notification(to_address, from_address)
+    mail( to: to_address,
+          from: from_address,
+          subject: 'New professional form submission from careerseekers.org.au',
+          template_name: 'professional_submission_notification' )
+  end
+
+  def company_submission_notification(to_address, from_address)
+    mail( to: to_address,
+          from: from_address,
+          subject: 'New company form submission from careerseekers.org.au',
+          template_name: 'company_submission_notification' )
+  end
 end
--- spec/mailers/previews/form_submission_mailer_preview.rb
+++ spec/mailers/previews/form_submission_mailer_preview.rb
@@ -1,4 +1,3 @@
 # Preview all emails at http://localhost:3000/rails/mailers/form_submission_mailer
 class FormSubmissionMailerPreview < ActionMailer::Preview
-
 end
