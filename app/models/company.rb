1bc6e63f44f371ce756e81a43c8c472e50d562ed
Nat Young <nat.young7@gmail.com>
2015-10-31 11:03:21 +1100

    basic validation

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,5 +3,8 @@ class Company
 
   attr_accessor :name, :contact_name, :phone, :email
 
+  validates :name, length: { maximum: 255 }
+  validates :contact_name, length: { maximum: 255 }
+  validates :phone, length: { maximum: 255 }
   validates :email, presence: true, length: { in: 2..255 }
 end
