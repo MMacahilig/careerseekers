bfd4c28de9dcb43ef0ceb3bbddf2ae3335d61b58
MMacahilig <michael.peter.macahilig@gmail.com>
2016-07-13 09:24:46 +1000

    added new profiles, modified the form, models, form submission files to merge participants streams into 1 form, changed background images to real students

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,12 +1,13 @@
 class Person
   include ActiveModel::Model
 
-  attr_accessor :first_name, :last_name, :phone, :email, :current_status, :degree, :preferred_placement_industry
+  attr_accessor :first_name, :last_name, :phone, :email, :stream, :current_status, :degree, :preferred_placement_industry
 
   validates :first_name, length: { maximum: 255 }
   validates :last_name, length: { maximum: 255 }
   validates :phone, length: { maximum: 255 }
   validates :email, presence: true, length: { in: 2..255 }
+  validates :stream, length: { maximum: 255 }
   validates :current_status, length: { maximum: 255 }
   validates :degree, length: { maximum: 75 }
   validates :preferred_placement_industry, length: { maximum: 255 }
